0fa0cf2657fc1433eebeef82724a7cbeb9f6ced5
Hemshree <31439735+Hemshree100@users.noreply.github.com>
2019-08-01 20:50:34 +0530

    Intro to conditional statements

--- Day3.py
+++ Day3.py
@@ -1,1 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+N = int(input())
+
+if N % 2 != 0:
+    print("Weird")
+else:
+    if N <= 5:
+        print("Not Weird")
+    elif N <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
 
